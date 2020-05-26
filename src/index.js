1c73f226c18fcbe4816ee4291b29061398989122
MrMaddy7 <30777026+MrMaddy7@users.noreply.github.com>
2020-05-26 14:09:17 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+//want to add new component just fill the data in the emojipedia.js file
+//which will automatically create a new card for the data which is put up
